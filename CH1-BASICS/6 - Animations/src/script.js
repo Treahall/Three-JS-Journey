e395604927ab655e96b02a93cffb6f54f7d67d24
Trever Hall <treahall@ut.utm.edu>
2021-12-28 17:22:21 -0600

    Lesson complete with learning how to animate using recursive calls to requestAnimationFrame and tweens using the gsap library

--- CH1-BASICS/6 - Animations/src/script.js
+++ CH1-BASICS/6 - Animations/src/script.js
@@ -1,5 +1,6 @@
 import './style.css'
 import * as THREE from 'three'
+import gsap from 'gsap'
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
@@ -29,4 +30,27 @@ const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
-renderer.render(scene, camera)
+
+// Clock
+const clock = new THREE.Clock()
+
+gsap.to(mesh.position, { duration: 1, delay: 1, x: 2})
+gsap.to(mesh.position, { duration: 1, delay: 2, x: 0})
+
+// Animations
+const tick = () => {
+    // Clock
+    // const elapsedTime = clock.getElapsedTime()
+
+    // // Update objects
+    // mesh.rotation.x = Math.cos(elapsedTime)
+    // mesh.rotation.y = Math.sin(elapsedTime)
+    // camera.lookAt(mesh.position)
+
+    // Render
+    renderer.render(scene, camera)
+
+    window.requestAnimationFrame(tick)
+}
+
+tick()
